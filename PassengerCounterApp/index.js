a267d2fb7484c4ef9714cc16e8c06a54f25bd19d
ak7662 <ak7662@gmail.com>
2021-06-15 17:06:33 +0530

    PassengerCounterApp

--- PassengerCounterApp/index.js
+++ PassengerCounterApp/index.js
@@ -1,3 +1,15 @@
-let count = 5
-count = 3
-console.log(count)
+const increment=document.querySelector(".increment")
+const count=document.querySelector(".count")
+let countValue = parseInt(count.textContent)
+const previousentries = document.querySelector(".previousentries")
+let entries ="Previous entries: "
+increment.addEventListener("click",()=>{
+    countValue++
+    count.textContent = countValue
+})
+function save(){
+    entries += countValue + " - "
+    previousentries.textContent = entries
+    countValue = 0
+    count.textContent = countValue
+}
