ebab543c9f8379708f55eddc55dfb1138dbdf7a6
ak7662 <ak7662@gmail.com>
2021-06-24 10:44:52 +0530

    blackjackGameCompleted

--- BlackJack_Game/index.js
+++ BlackJack_Game/index.js
@@ -1,15 +1,36 @@
-let firstCard = 6
-let secondCard = 6
-let cards = [firstCard,secondCard]
-let sum = firstCard+secondCard
+
+let user = {
+    name: "Abhishek",
+    credits: "$110"
+}
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 const messageEl = document.getElementById("message-el")
 const sumEl = document.getElementById("sum-el")
 const cardsEl = document.getElementById("cards-el")
+const userDetails = document.getElementById("userDetails")
+userDetails.textContent = user.name+": "+user.credits
 
-const startGame = () => renderGame()
+function getRandomCard(){
+    let randomNumber = Math.floor(Math.random()*13)+1
+    const returnValue = randomNumber === 1? 11 : randomNumber > 10 ? 10 : randomNumber
+    return returnValue
+}
+const startGame = () => {
+    if(!isAlive)
+    {
+        isAlive = true
+        let firstCard = getRandomCard()
+        let secondCard = getRandomCard()
+        cards = [firstCard,secondCard]
+    }
+    sum = cards[0] + cards[1]
+    renderGame()
+    
+}
 const renderGame = () =>{
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -31,8 +52,11 @@ const renderGame = () =>{
 }
 
 const newCard = () =>{
-    newCardVarible = 9
-    cards.push(newCardVarible)
-    sum += newCardVarible
-    startGame()
+    if(isAlive && !hasBlackJack)
+    {
+        newCardVarible = getRandomCard()
+        cards.push(newCardVarible)
+        sum += newCardVarible
+        renderGame()
+}
 }
