f106f679b503c3bef8427f234477e0c53897f580
ak7662 <ak7662@gmail.com>
2021-06-24 08:35:02 +0530

    blackjack game updated

--- BlackJack_Game/index.js
+++ BlackJack_Game/index.js
@@ -1,15 +1,38 @@
-const increment=document.querySelector(".increment")
-const count=document.querySelector(".count")
-let countValue = parseInt(count.textContent)
-const previousentries = document.querySelector(".previousentries")
-let entries ="Previous entries: "
-increment.addEventListener("click",()=>{
-    countValue++
-    count.textContent = countValue
-})
-function save(){
-    entries += countValue + " - "
-    previousentries.textContent = entries
-    countValue = 0
-    count.textContent = countValue
+let firstCard = 6
+let secondCard = 6
+let cards = [firstCard,secondCard]
+let sum = firstCard+secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+const messageEl = document.getElementById("message-el")
+const sumEl = document.getElementById("sum-el")
+const cardsEl = document.getElementById("cards-el")
+
+const startGame = () => renderGame()
+const renderGame = () =>{
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+    let cardtext = ""
+    for(card of cards){
+        cardtext += card + " "
+    }
+    cardsEl.textContent = "Cards: " +cardtext
+    sumEl.textContent = "Sum: " + sum
+
+}
+
+const newCard = () =>{
+    newCardVarible = 9
+    cards.push(newCardVarible)
+    sum += newCardVarible
+    startGame()
 }
