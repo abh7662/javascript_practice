d17ecd11dfd11499fe30c83a78a609bc1f655801
ak7662 <ak7662@gmail.com>
2021-06-25 22:44:46 +0530

    cherome extension completed

--- Build_Chrome_Extension/index.js
+++ Build_Chrome_Extension/index.js
@@ -1,19 +1,33 @@
-let myLeads = ["wwww.google.com","wwww.google.com","wwww.google.com","wwww.google.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const tabBtn = document.getElementById("tab-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEL = document.getElementById("ul-el")
 inputBtn.addEventListener("click",handleClick)
-renderLeads() 
-function handleClick(){
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()   
+tabBtn.addEventListener("click",handleTab)
+deleteBtn.addEventListener("dblclick",handleDelete)
+// const tabs = [
+//     {url: "https://www.google.com"}
+// ]
+
+// localStorage.clear()
+const leadsfromlocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+// console.log(leadsfromlocalStorage)
+if(leadsfromlocalStorage){
+    myLeads = leadsfromlocalStorage
+    render(myLeads)
 }
-function renderLeads(){
-    // console.log(myLeads)
-    // const liEl = document.createElement("li")
-    // liEl.textContent = inputEl.value
-    // ulEL.append(liEl)
+function handleTab(){
+    // console.log(tabs[0].url)
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+    
+}
+function render(myLeads){
     let htmlText = ""
     for(lead of myLeads){
         htmlText += `
@@ -24,5 +38,18 @@ function renderLeads(){
             </li>`
     }
     ulEL.innerHTML = htmlText
-    
 }
+function handleDelete(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+}
+function handleClick(){
+    if(inputEl.value){
+        myLeads.push(inputEl.value)
+        inputEl.value = ""
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        render(myLeads)   
+    }
+}
+
